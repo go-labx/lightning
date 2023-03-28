7e67e3874d7fbc4b977b552ca607f223bb34399b
wanggan.kk <wanggan.kk@bytedance.com>
2023-03-28 17:55:59 +0800

    chore: add example hello

---
+++ examples/hello/app.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/go-labx/lightning"
+
+func main() {
+	app := lightning.App()
+
+	app.Get("/", func(ctx *lightning.Context) {
+		ctx.JSON(map[string]string{
+			"message": "hello world~",
+		})
+	})
+
+	app.Run(":8080")
+}
