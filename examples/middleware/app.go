a08e1438b8246d68076e8ea3313bfd5f7e8d8e3b
wanggan.kk <wanggan.kk@bytedance.com>
2023-03-30 08:54:00 +0800

    chore: update example middleware

--- examples/middleware/app.go
+++ examples/middleware/app.go
@@ -9,27 +9,35 @@ func main() {
 	app := lightning.App()
 
 	// Global scope middleware
-	app.Use(func(ctx *lightning.Context, next lightning.Next) {
-		fmt.Println("global scope middleware --->")
-		next()
-		fmt.Println("<--- global scope middleware")
+	app.Use(func(ctx *lightning.Context) {
+		fmt.Println("global scope middleware 1 --->")
+		ctx.Next()
+		fmt.Println("<--- global scope middleware 1")
+	})
+	app.Use(func(ctx *lightning.Context) {
+		fmt.Println("global scope middleware 2 --->")
+		ctx.Next()
+		fmt.Println("<--- global scope middleware 2")
+	})
+	app.Use(func(ctx *lightning.Context) {
+		fmt.Println("global scope middleware 3 --->")
+		ctx.Next()
+		fmt.Println("<--- global scope middleware 3")
 	})
 
-	app.Get("/ping", func(ctx *lightning.Context) {
-		ctx.Text("pong")
+	app.Get("/", func(ctx *lightning.Context) {
+		ctx.JSON(map[string]string{
+			"message": "hello world",
+		})
 	})
 
 	// Route scope middleware
-	app.Get("/hello", func(ctx *lightning.Context) {
-		ctx.Text("hello world")
-	}, func(ctx *lightning.Context, next lightning.Next) {
-		fmt.Println("Route scope middleware 1 --->")
-		next()
-		fmt.Println("<--- Route scope middleware 1")
-	}, func(ctx *lightning.Context, next lightning.Next) {
-		fmt.Println("Route scope middleware 2 --->")
-		next()
-		fmt.Println("<--- Route scope middleware 2")
+	app.Get("/ping", func(ctx *lightning.Context) {
+		fmt.Println("route scope middleware --->")
+		ctx.Next()
+		fmt.Println("<--- route scope middleware")
+	}, func(ctx *lightning.Context) {
+		ctx.Text("pong")
 	})
 
 	app.Run()
