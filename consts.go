f43dfc8feba2df01e76d5ba9c5b44e10f146dbfc
kk0829 <tzhwg0829@126.com>
2023-04-18 21:42:04 +0800

    chore: add consts

--- consts.go
+++ consts.go
@@ -1,11 +1,13 @@
 package lightning
 
+// MIME types
 const (
 	MIMETextPlain       = "text/plain"
 	MIMEApplicationXML  = "application/xml"
 	MIMEApplicationJSON = "application/json"
 )
 
+// Header keys
 const (
 	HeaderContentType        = "Content-Type"
 	HeaderContentDisposition = "Content-Disposition"
