487d089a0afd996a78a905ed49caf67d0592724c
kk0829 <tzhwg0829@126.com>
2023-04-22 09:42:23 +0800

    chore: update response header example code

--- examples/response_header/app.go
+++ examples/response_header/app.go
@@ -15,11 +15,11 @@ func main() {
 		ctx.AddHeader("foo", "baz")
 		ctx.AddHeader("foo", "baq")
 
-		// delete all headers with key "foo"
-		ctx.DelHeader("foo")
-
 		// set a header with key "id" and value "ewh2mime9purchaser4error"
 		ctx.SetHeader("id", "ewh2mime9purchaser4error")
+
+		// delete all headers with key "bar"
+		ctx.DelHeader("bar")
 
 		ctx.JSON(http.StatusOK, lightning.Map{
 			"message": "pong",
