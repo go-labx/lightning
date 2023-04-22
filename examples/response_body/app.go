c83945974acd4ff84351cbd2e6d0d71f15173e77
kk0829 <tzhwg0829@126.com>
2023-04-22 09:58:38 +0800

    chore: update response body example

--- examples/response_body/app.go
+++ examples/response_body/app.go
@@ -16,24 +16,30 @@ func main() {
 	app := lightning.DefaultApp()
 
 	app.Get("/text", func(ctx *lightning.Context) {
-		ctx.Text(http.StatusOK, "hello world") // Return "hello world" as plain text
+		// Return "hello world" as plain text
+		ctx.Text(http.StatusOK, "hello world")
 	})
 
 	app.Get("/json", func(ctx *lightning.Context) {
-		ctx.JSON(http.StatusOK, lightning.Map{
-			"message": "pong", // Return a JSON object with a "message" key and "pong" value
+		// Return a Person object as JSON with name "zhangsan", age 20, and city "Hangzhou"
+		ctx.JSON(http.StatusOK, &Person{
+			Name: "zhangsan",
+			Age:  20,
+			City: "Hangzhou",
 		})
 	})
 
 	app.Get("/xml", func(ctx *lightning.Context) {
-		ctx.XML(http.StatusOK, Person{
-			Name: "zhangsan", // Return a Person object as XML with name "zhangsan", age 20, and city "Hangzhou"
+		// Return a Person object as XML with name "zhangsan", age 20, and city "Hangzhou"
+		ctx.XML(http.StatusOK, &Person{
+			Name: "zhangsan",
 			Age:  20,
 			City: "Hangzhou",
 		})
 	})
 
 	app.Get("/file", func(ctx *lightning.Context) {
+		// Serve the README.md file
 		err := ctx.File("./README.md")
 		if err != nil {
 			ctx.Fail(-1, err.Error())
@@ -41,15 +47,17 @@ func main() {
 	})
 
 	app.Get("/success", func(ctx *lightning.Context) {
+		// Return a successful response with a Person object as JSON with name "zhangsan", age 20, and city "Hangzhou"
 		ctx.Success(&Person{
-			Name: "zhangsan", // Return a successful response with a Person object as JSON with name "zhangsan", age 20, and city "Hangzhou"
+			Name: "zhangsan",
 			Age:  20,
 			City: "Hangzhou",
 		})
 	})
 
 	app.Get("/fail", func(ctx *lightning.Context) {
-		ctx.Fail(9999, "network error") // Return a failure response with code 9999 and message "network error"
+		// Return a failure response with code 9999 and message "network error"
+		ctx.Fail(9999, "network error")
 	})
 
 	// Start the server on port 6789
